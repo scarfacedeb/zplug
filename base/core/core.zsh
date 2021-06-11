232bd3f713c771628a704c2e12dddd9f025afedf
Andrew Volozhanin <scarfacedeb@gmail.com>
2021-06-11 18:34:38 +0200

    Comment git/awk/zsh version checks to speed up start up

--- base/core/core.zsh
+++ base/core/core.zsh
@@ -92,36 +92,38 @@ __zplug::core::core::prepare()
     # 1. zsh 4.3.9 or more
     # 2. git
     # 3. nawk or gawk
-    {
-        if ! __zplug::base::base::zsh_version 4.3.9; then
-            __zplug::io::print::f \
-                --die \
-                --zplug \
-                --error \
-                "zplug does not work this version of zsh $ZSH_VERSION.\n" \
-                "You must use zsh 4.3.9 or later.\n"
-            return 1
-        fi
+    #
+    # HACK: to speed up start up
+    # {
+    #     if ! __zplug::base::base::zsh_version 4.3.9; then
+    #         __zplug::io::print::f \
+    #             --die \
+    #             --zplug \
+    #             --error \
+    #             "zplug does not work this version of zsh $ZSH_VERSION.\n" \
+    #             "You must use zsh 4.3.9 or later.\n"
+    #         return 1
+    #     fi
 
-        if ! __zplug::base::base::git_version 1.7; then
-            __zplug::io::print::f \
-                --die \
-                --zplug \
-                --error \
-                "git command not found in \$PATH\n" \
-                "zplug depends on git 1.7 or later.\n"
-            return 1
-        fi
+    #     if ! __zplug::base::base::git_version 1.7; then
+    #         __zplug::io::print::f \
+    #             --die \
+    #             --zplug \
+    #             --error \
+    #             "git command not found in \$PATH\n" \
+    #             "zplug depends on git 1.7 or later.\n"
+    #         return 1
+    #     fi
 
-        if ! __zplug::utils::awk::available; then
-            __zplug::io::print::f \
-                --die \
-                --zplug \
-                --error \
-                'No available AWK variant in your $PATH\n'
-            return 1
-        fi
-    }
+    #     if ! __zplug::utils::awk::available; then
+    #         __zplug::io::print::f \
+    #             --die \
+    #             --zplug \
+    #             --error \
+    #             'No available AWK variant in your $PATH\n'
+    #         return 1
+    #     fi
+    # }
 
     # Release zplug variables and export
     __zplug::core::core::variable || return 1
